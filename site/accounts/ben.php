56012a25ba32993a2369c1b472054c590d55dc5d
alexabbott <alex.r.abbott@gmail.com>
2016-04-25 21:57:44 -0400

    Add new user account

---
+++ site/accounts/ben.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: ben
+firstname: ""
+lastname: ""
+email: benmbenjamin@gmail.com
+password: >
+  $2a$10$mAyLTZ2HvlZy4VSdAi2VxeF.i6zjUnpFLoeI7zILYI0CGMf94Ndt2
+language: en
+role: admin
