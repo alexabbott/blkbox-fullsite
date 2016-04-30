581c6fe4a225798daf6b11231e6f1cf1a3c7ce9b
benBen <benmbenjamin@gmail.com>
2016-04-30 16:41:22 -0700

    contact: everything is jank but dont worry..

--- site/blueprints/contact.php
+++ site/blueprints/contact.php
@@ -16,3 +16,24 @@ fields:
   subtext:
     label: Subtext
     type:  textarea
+  box1:
+    label: Box 1, Title
+    type: textarea
+  box1Sub:
+    label: Box 1, Subtitle
+    type: text
+  box2:
+    label: Box 2, Title
+    type: textarea
+  box2Sub:
+    label: Box 2, Subtitle
+    type: text
+  box3:
+    label: Box 3, Title
+    type: textarea
+  box3Sub:
+    label: Box 3, Subtitle
+    type: text
+  cta:
+    label: Call To Action
+    type: textarea
--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,17 +1,36 @@
 <?php snippet('header') ?>
 
-<main class="contactContent"
-      style="background-image: url('<?php echo $kirby->urls()->assets() ?>/images/contact.png')">
+<main class="contactContent">
+  <span class="absolute imgCont">
+    <img class="absolute" src="<?php echo $kirby->urls()->assets() ?>/images/contact.png" />
+  </span>
+  <span class="background-mesh"></span>
 
-  <h1 class="uppercase bold white text-center pageTitle"><?php echo $page->header()->html() ?></h1>
+  <h1 class="uppercase bold white text-center relative pageTitle"><?php echo $page->header()->html() ?></h1>
 
-  <div id="boxCont" class="flex justify-between">
-    <span class="box">
+  <div id="boxCont" class="flex justify-center relative">
+    <span class="box pointer">
+      <h1 class="uppercase"><?php echo $page->box1()->html() ?></h1>
+      <span class="block textCenter"><?php echo $page->box1Sub()->html() ?></span>
     </span>
-    <span class="box">
+    <span class="box pointer">
+      <h1 class="uppercase"><?php echo $page->box2()->html() ?></h1>
+      <span class="block textCenter"><?php echo $page->box2Sub()->html() ?></span>
     </span>
-    <span class="box">
+    <span class="box pointer">
+      <h1 class="uppercase"><?php echo $page->box3()->html() ?></h1>
+      <span class="block textCenter"><?php echo $page->box3Sub()->html() ?></span>
     </span>
+  </div>
+
+  <div id="contactCTA" class="relative text-center white">
+    <span class="uppercase">
+      <?php echo $page->cta()->html() ?>
+    </span>
+    <form>
+      <input type="text" placeholder="email" />
+      <input type="submit"
+    </form>
   </div>
 </main>
 
