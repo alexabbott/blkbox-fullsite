36aa123d86323220fd56e766e6c32a31cd61b07e
alexabbott <alex.r.abbott@gmail.com>
2016-05-03 21:30:22 -0400

    Update homepage functionality, mobile styling, etc.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,18 @@ $(document).ready(function(){
 		$('main-container').toggleClass('fixed');
 	});
 
+	$('.video-overlay').mousedown(function() {
+		var giffy = $('.get-animation').attr('data-src');
+		$('.get-animation').css({'display':'block'});
+		setTimeout(function(){
+			$('.get-animation').css({'opacity':'1'});
+		}, 200);
+		$('.get-animation').attr('src', giffy + "?a=" + Math.random());
+		$(this).mouseup(function() {
+			window.location.pathname += 'about';
+		});
+	});
+
 	// expand square
 	var divMouseDown;
 	$('.square').on('mousedown touchstart', function() {
