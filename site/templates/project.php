bba3561b12ebd1f3b8469169d5ba038626870290
James Ewing <olearysound@gmail.com>
2016-05-03 21:20:02 -0700

    Adjusted case studies according to client spec. Changed arrow to font-awesome icon, moved to mid-vertical position (Franks)

--- site/templates/project.php
+++ site/templates/project.php
@@ -10,17 +10,17 @@
       <h3><?php echo $page->title()->html() ?></h3>
       <h1 class="headline"><?php echo $page->headline()->html() ?></h1>
       <h3 class="subtitle"><?php echo $page->subtitle()->html() ?></h3>
-      <div class="project-desc">
-        <?php echo $page->text()->kirbytext() ?>
-      </div>
       <nav class="nextprev cf" role="navigation">
         <?php if($prev = $page->prevVisible()): ?>
         <a class="prev" href="<?php echo $prev->url() ?>">&larr;</a>
         <?php endif ?>
         <?php if($next = $page->nextVisible()): ?>
-        <a class="next" href="<?php echo $next->url() ?>">&rarr;</a>
+        <a class="next fa fa-angle-right" href="<?php echo $next->url() ?>" aria-hidden="true"></a>
         <?php endif ?>
       </nav>
+      <div class="project-desc">
+        <?php echo $page->text()->kirbytext() ?>
+      </div>
     </div>
 
 
