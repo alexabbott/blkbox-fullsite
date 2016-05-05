c92b64727273cbd7ac8d329c3c6687440aa890e0
James Ewing <olearysound@gmail.com>
2016-05-04 17:50:16 -0700

    adjusting hover effect for work <li> text-transformations, per client spec on .psd

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,16 +1,19 @@
 <ul class="teaser cf">
   <?php foreach(page('work')->children()->visible()->limit(5) as $project): ?>
   <li class="<?php echo $project->slug() ?>">
-    <h3><?php echo $project->title()->html() ?></h3>
+    <h3 id="workHeader"><?php echo $project->title()->html() ?></h3>
     <h1 class="headline"><a href="<?php echo $project->url() ?>"><?php echo $project->headline()->html() ?></a></h1>
-    <h3 id="teaserSubtitle" class="subtitle"><?php echo $project->subtitle()->html() ?></h3>
-    <br>
-    <!-- <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3> -->
     <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
     <a href="<?php echo $project->url() ?>" class="project-image-holder">
       <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
     </a>
     <?php endif ?>
+    <h3 id="teaserSubtitle" class="subtitle"><?php echo $project->subtitle()->html() ?></h3>
+    <br>
+    <!-- <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3> -->
   </li>
   <?php endforeach ?>
 </ul>
+
+
+
