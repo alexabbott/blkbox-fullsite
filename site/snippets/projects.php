6253084a77d36436930c3086b55f504683ea7efb
James Ewing <olearysound@gmail.com>
2016-05-04 19:07:31 -0700

    adjusted work page hover features to show content according to client spec.

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -2,7 +2,7 @@
   <?php foreach(page('work')->children()->visible()->limit(5) as $project): ?>
   <li class="<?php echo $project->slug() ?>">
     <h3 id="workHeader"><?php echo $project->title()->html() ?></h3>
-    <h1 class="headline"><a href="<?php echo $project->url() ?>"><?php echo $project->headline()->html() ?></a></h1>
+    <h1 id="workHeadline" class="headline"><a href="<?php echo $project->url() ?>" id="workHeadlink"><?php echo $project->headline()->html() ?></a></h1>
     <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
     <a href="<?php echo $project->url() ?>" class="project-image-holder">
       <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
