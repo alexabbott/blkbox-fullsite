3e6f676d79c777ae1095732cd6780ec5fa983291
James Ewing <olearysound@gmail.com>
2016-05-05 14:49:48 -0700

    Removed 'works' header for new work projects page.  Working on adding client list to bottom.

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -13,7 +13,36 @@
     <!-- <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3> -->
   </li>
   <?php endforeach ?>
+  
+  <li>
+
+    <!-- <div class="row clients-header"> -->
+      <h3 id="clientList">Clients</h3>
+    <!-- </div> -->
+
+  </li>
+
 </ul>
 
+<!-- <div class="client-list">
+  <div class="row">   
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/baxalta.jpg" alt="Baxalta">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/beachbody.jpg" alt="Beachbody">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/bonotel.jpg" alt="Bonotel">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/brock.jpg" alt="Brock">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/facebook.jpg" alt="Facebook">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/franks.jpg" alt="Franks">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/frenchs.jpg" alt="French'">
+  </div>
+  <div class="row">   
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/garden.jpg" alt="Garden Lites">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/havas.jpg" alt="Havas">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/hiring.jpg" alt="Hiring our Heroes">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/melange.jpg" alt="Melange Expressions">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/piercing.jpg" alt="Piercing Pagoda">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/tacobell.jpg" alt="Taco Bell">
+    <img src="<?php echo $kirby->urls()->assets() ?>/images/logos/western.jpg" alt="Western Union">
+  </div>
+</div> -->
 
 
