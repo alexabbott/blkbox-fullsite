331b5c83aef3611271370358fc1b511aa555d50d
benBen <benmbenjamin@gmail.com>
2016-05-09 01:04:17 -0700

    adjusts margin of nav elements to match footer elements, contact flex

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -28,26 +28,14 @@
     <!-- Begin MailChimp Signup Form -->
     <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
     <style type="text/css">
-      #mc_embed_signup{background: transparent; clear:left; font:14px 'futura-pt',Arial,sans-serif; margin: 0 auto; text-transform: uppercase; width: 400px;}
-      #mc_embed_signup .mc-field-group { display: inline-block; width: 74%; }
-      #mc_embed_signup .mc-field-group input { border-radius: 0; height: 33px; }
-      #mc_embed_signup .clear { display: inline-block; margin-top: 6px; vertical-align: middle; }
-      #mc_embed_signup .button { background: #fff; border: 2px solid #fff; border-radius: 0; color: #000; font:14px 'futura-pt',Arial,sans-serif; font-weight: 600; letter-spacing: -1px; text-transform: uppercase; transition: .2s ease-out; }
-      #mc_embed_signup .button:hover { background: transparent; color: #fff; }
-      #mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error { display: none !important;}
-      @media screen and (max-width: 767px) {
-        #mc_embed_signup {
-          width: 305px;
-        }
-        #mc_embed_signup .mc-field-group {
-          width: 64%;
-        }
+      div.mce_inline_error {
+        display: none;
       }
     </style>
     <div id="mc_embed_signup">
     <form action="//weareblkbox.us13.list-manage.com/subscribe/post?u=66c0110c974d571246ac52630&amp;id=004da7f262" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
-      
+
     <div class="mc-field-group">
       <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="email address">
     </div>
